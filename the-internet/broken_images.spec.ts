d3952a579ba02a0d9158d2e413e815ad89584caa
estevaolelis <estevaolelis@gmail.com>
2026-04-26 16:00:37 -0300

    feat: broken images automation

---
+++ the-internet/broken_images.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+test("Validadate how many broken images", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.getByRole("link", { name: "Broken Images" }).click();
+
+  const images = page.locator("img")
+  const count = await images.count()
+
+  const brokenImages: string[] = []
+
+  for (let i = 0; i < count; i++){
+    const image = images.nth(i)
+
+    const isBroken = await image.evaluate((img: HTMLImageElement) => img.naturalWidth === 0)
+
+    if (isBroken) {
+        const source = await image.getAttribute('src')
+        brokenImages.push(source || "sem-src")
+    }
+
+    expect(brokenImages.length).toBe(0)
+  }
+});
