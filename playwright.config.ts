3acdd4f9939b6dc7ba0db1d475f3ced3aaad1fb5
estevaolelis <estevaolelis@gmail.com>
2026-04-26 15:54:17 -0300

    feat: env example and basic auth automation

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config()
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './the-internet',
   /* Run tests in files in parallel */
@@ -25,11 +17,13 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: process.env.BASE_URL,
     trace: 'on-first-retry',
+
+    httpCredentials: {
+      username: process.env.USER_AUTH!,
+      password: process.env.PASS_AUTH!,
+    }
   },
 
   /* Configure projects for major browsers */
