e1418b350524b1c6101dd1219d843fdf8dc8f3d9
estevaolelis <estevaolelis@gmail.com>
2026-04-27 15:25:56 -0300

    fix: fallback in playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'https://the-internet.herokuapp.com',
     trace: 'on-first-retry',
 
     httpCredentials: {
